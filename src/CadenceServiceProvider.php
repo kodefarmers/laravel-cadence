c895a17da78f4b6411c5b86a72323b7f6e04435a
Shivaji Chalise <respond2shivaji@gmail.com>
2026-07-17 12:59:08 +0545

    chore: fix PHPStan issues for CadenceServiceProvider

--- src/CadenceServiceProvider.php
+++ src/CadenceServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kodefarmers\Cadence;
 
 use Illuminate\Contracts\Cache\Factory as CacheFactory;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use Kodefarmers\Cadence\Contracts\StateRepository;
@@ -20,15 +21,35 @@ class CadenceServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/cadence.php', 'cadence');
 
-        $this->app->singleton(CadenceConfig::class, fn (Application $app) => new CadenceConfig(
-            freeAttempts: (int) $app['config']->get('cadence.free_attempts'),
-            idleTimeout: (int) $app['config']->get('cadence.idle_timeout'),
-        ));
+        $this->app->singleton(CadenceConfig::class, function (Application $app): CadenceConfig {
+            /** @var ConfigRepository $config */
+            $config = $app->make(ConfigRepository::class);
 
-        $this->app->singleton(StateRepository::class, fn (Application $app) => new CacheStateRepository(
-            cache: $app[CacheFactory::class]->store(),
-            config: $app[CadenceConfig::class],
-        ));
+            /** @var int $freeAttempts */
+            $freeAttempts = $config->get('cadence.free_attempts');
+
+            /** @var int $idleTimeout */
+            $idleTimeout = $config->get('cadence.idle_timeout');
+
+            return new CadenceConfig(
+                freeAttempts: $freeAttempts,
+                idleTimeout: $idleTimeout,
+            );
+        });
+
+
+        $this->app->singleton(StateRepository::class, function (Application $app): CacheStateRepository {
+            /** @var CacheFactory $cacheFactory */
+            $cacheFactory = $app->make(CacheFactory::class);
+
+            /** @var CadenceConfig $config */
+            $config = $app->make(CadenceConfig::class);
+
+            return new CacheStateRepository(
+                cache: $cacheFactory->store(),
+                config: $config
+            );
+        });
 
         $this->app->singleton(
             CadenceManager::class,
